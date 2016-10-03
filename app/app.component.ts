79a38c69bfc47b4d751bbd5ad316c80d99b5a79d
jay raparla <dhananjay.raparla@gmail.com>
2016-10-03 16:19:01 -0400

    added heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,34 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: 'app/pages/test.html'
+    // template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
+  heroes = HEROES;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
